7bc36eaf18e74bf86c131628599775ca77413de6
Marcin Pazgier <mpazgier@yahoo.co.uk>
2024-03-07 23:23:59 +0100

    first migration added with indexes

---
+++ PowerAnaliticPoC.Infrastructure/Persistance/EFRepository/PowerAnaliticsDBContext.cs
@@ -0,0 +1,18 @@
+﻿using PowerAnaliticPoC.Domain.PowerGenerator;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerAnaliticPoC.Infrastructure.Persistance.EFRepository
+{
+    public class PowerAnaliticsDBContext: DbContext
+    {
+        public DbSet<PowerGenerator> PowerGenerators { get; set; }
+        public DbSet<PowerGeneratorDetailData> PowerGeneratorDetailData { get; set; }   
+        public DbSet<PowerGeneratorTimeRangeData> PowerGeneratorTimeRangeData { get; set; }
+    }
+
+}
